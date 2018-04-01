373ea662c2086079b1f5ca9ced9856c1ff6c41e2
Rik Brown <rik@rik.codes>
2018-03-31 18:06:04 -0700

    Don't assume any profile exists (we'll inject via docker)

--- src/main/kotlin/codes/rik/dynamicdnsupdater/App.kt
+++ src/main/kotlin/codes/rik/dynamicdnsupdater/App.kt
@@ -1,15 +1,12 @@
 package codes.rik.dynamicdnsupdater
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.route53.AmazonRoute53ClientBuilder
 
 /**
  * Application runner
  */
 object App {
-    private val route53 = AmazonRoute53ClientBuilder.standard()
-            .withCredentials(ProfileCredentialsProvider("rik-systems-dynamic-dns-updater"))
-            .build()
+    private val route53 = AmazonRoute53ClientBuilder.defaultClient()
     private val dnsUpdater = Route53DnsUpdater(route53,
             hostedZoneId = "Z39HKRRJQ8AJ0",
             record = "seattle.rik.systems",
