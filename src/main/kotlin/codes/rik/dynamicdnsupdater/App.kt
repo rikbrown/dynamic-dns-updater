cbcbbdc0bef8f3cf961a45f7a911fbe03b17f626
Rik Brown <rik@rik.codes>
2018-03-31 19:17:39 -0700

    us-west-2 default

--- src/main/kotlin/codes/rik/dynamicdnsupdater/App.kt
+++ src/main/kotlin/codes/rik/dynamicdnsupdater/App.kt
@@ -1,12 +1,15 @@
 package codes.rik.dynamicdnsupdater
 
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.route53.AmazonRoute53ClientBuilder
 
 /**
  * Application runner
  */
 object App {
-    private val route53 = AmazonRoute53ClientBuilder.defaultClient()
+    private val route53 = AmazonRoute53ClientBuilder.standard()
+            .withRegion(Regions.US_WEST_2)
+            .build()
     private val dnsUpdater = Route53DnsUpdater(route53,
             hostedZoneId = "Z39HKRRJQ8AJ0",
             record = "seattle.rik.systems",
